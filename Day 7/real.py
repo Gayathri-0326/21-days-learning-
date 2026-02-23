1be9b1820f96fdbdcf7ff0bb4bab968b312d7760
Gayathri-0326 <gayathrikaviya333@gmail.com>
2026-02-23 20:29:21 +0530

    program

---
+++ Day 7/real.py
@@ -0,0 +1,9 @@
+print("Studendt mark analysis")
+marks=[80,90,99,84,100]
+total=sum(marks)
+average=total/len(marks)
+print("Average marks=",average)
+print("Attendance tracker")
+attendance=["P","A","P","P","A"]
+present=attendance.count("P")
+print("Students present=",present)
