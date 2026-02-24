b056f08ba1882604d223917f10239cef14f0254f
Gayathri-0326 <gayathrikaviya333@gmail.com>
2026-02-24 19:17:09 +0530

    program

---
+++ Day 8/tuple.py
@@ -0,0 +1,20 @@
+'''print("Student Record")
+name=input("Enter your name:")
+age=input("Enter your age:")
+department=input("Enter your department:")
+sheet=(name,age,department)
+for i in sheet:
+    print(i)'''
+'''print("Temperature Record")
+morning=float(input("Enter morning temperature:"))
+afternoon=float(input("Enter afternoon temperature:"))
+night=float(input("Enter night temperature:"))
+temp=(morning,afternoon,night)
+average=sum(temp)/len(temp)
+print("Average temperature=",average)'''
+print("Product information")
+name=input("Enter product name:")
+price=input("Enter product price:")
+detail=(name,price)
+print("Product=",name,"\nPrice=",price)
+
