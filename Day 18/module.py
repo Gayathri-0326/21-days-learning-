23025090e4fa243d86cae737c9ae6bf7ec2670ab
Gayathri-0326 <gayathrikaviya333@gmail.com>
2026-03-08 20:47:11 +0530

    program.py

---
+++ Day 18/module.py
@@ -0,0 +1,16 @@
+print("Square root")
+import math
+num = float(input("Enter a number: "))
+result = math.sqrt(num)
+print("Square root =", result)
+print("Random Number 1 to 100")
+import random
+number = random.randint(1, 100)
+print("Random number:", number)
+print("Finding area of circle")
+import math
+import random
+radius = random.randint(1, 10)
+print("Random radius:", radius)
+area = math.pi * radius * radius
+print("Area of circle =", area)
