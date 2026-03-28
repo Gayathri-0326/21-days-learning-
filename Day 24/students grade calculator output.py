fc2bfb4144b68472c227f62b77bd1275c1851b52
Gayathri-0326 <gayathrikaviya333@gmail.com>
2026-03-28 19:17:06 +0530

    output

---
+++ Day 24/students grade calculator output.py
@@ -0,0 +1,17 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=============================================================== RESTART: C:/Users/Gayathri K/students grade calculator.py ==============================================================
+Enter student name: Kaviya
+Enter number of subjects: 5
+Enter mark for subject 1: 90
+Enter mark for subject 2: 96
+Enter mark for subject 3: 92
+Enter mark for subject 4: 99
+Enter mark for subject 5: 100
+
+--- Result ---
+Name: Kaviya
+Total Marks: 477
+Average: 95.4
+Grade: A
