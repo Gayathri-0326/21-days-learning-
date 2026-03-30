c6c6a57b9913efb548db5ee2f9709109b7967cea
Gayathri-0326 <gayathrikaviya333@gmail.com>
2026-03-30 21:40:54 +0530

    armstrong output

---
+++ Day 25/armstrongout.py
@@ -0,0 +1,10 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Gayathri K/armstrong.py
+Enter a number: 143
+Not an Armstrong number
+>>> 
+========== RESTART: C:/Users/Gayathri K/armstrong.py ==========
+Enter a number: 153
+Armstrong number
